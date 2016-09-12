0548b800a65968743934a0a5c40d003e502d314a
Pbra1484 <pbra1484@740s-pl-108.local>
2016-09-12 13:43:04 -0600

    tewaks

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -35,7 +35,7 @@ public class StupidController
 		readKeyboardInput.nextInt();
 		
 		System.out.println("Enter a number");
-		Double userInputQuestion3 = readKeyboardInput.nextDouble();
+		double userInputQuestion3 = readKeyboardInput.nextDouble();
 		System.out.println("The result is " + userInputQuestion3);
 		readKeyboardInput.nextLine();
 		
@@ -46,6 +46,22 @@ public class StupidController
 		System.out.println("If you typed more than a word it will apear here");
 		String userInputExtra = readKeyboardInput.nextLine();
 		System.out.println(userInputExtra);
+		
+		System.out.println("Type somting to fill up space");
+		String userInputQuestion5 = readKeyboardInput.nextLine();
+		System.out.println("You typed" + userInputQuestion5);
+		
+		System.out.println("How was your day?");
+		String userInputQuestion6 = readKeyboardInput.nextLine();
+		System.out.println("My day was " + userInputQuestion6 + " too");
+		
+		System.out.println("This is the end any final toughts?");
+		String userInputQuestion7 = readKeyboardInput.nextLine();
+		System.out.println("To repeat you typed " + userInputQuestion7);
+		
+		
+		
+		
 		
 	}
 }
